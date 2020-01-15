fae4478ae21b6d6dd31016047afd804aa1a58edc
Manal Alkandari <manal_alkandri@hotmail.com>
2020-01-15 20:14:36 +0300

    test07

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
 class Restaurant(models.Model):
-    name = models.CharField(max_length=120)
-    description = models.TextField()
-    opening_time = models.TimeField()
-    closing_time = models.TimeField()
+    class Meta"
+    	name = models.CharField(max_length=120)
+    	description = models.TextField()
+    	opening_time = models.TimeField()
+    	closing_time = models.TimeField()
